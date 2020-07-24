c1cde32a3572bfd43a14abd3cab74afc41d9044d
Demon000 <demonsingur@gmail.com>
2020-07-24 21:48:54 +0300

    input: nt36xxx: remove tests
    [@kardebayan: Remove oppo tests from driver]
    
    Co-authored-by: Debayan Kar <kardebayan3@gmail.com>

--- drivers/input/touchscreen/NT36525B/nt36xxx.h
+++ drivers/input/touchscreen/NT36525B/nt36xxx.h
@@ -102,7 +102,6 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 //---Customerized func.---
 #define NVT_TOUCH_PROC 1
 #define NVT_TOUCH_EXT_PROC 1
-#define NVT_TOUCH_MP 1
 #define MT_PROTOCOL_B 1
 #define NVT_TOUCH_FW_DEBUG_INFO 1
 #define WAKEUP_GESTURE 1
