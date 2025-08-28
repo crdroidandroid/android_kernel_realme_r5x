add7341dc1f8f7f1f8d0fcf821a7bba3b6493a93
Debayan Kar <kardebayan3@gmail.com>
2025-08-28 20:46:34 +0530

    build.sh: Switch to clang 20

--- build.sh
+++ build.sh
@@ -8,8 +8,7 @@ TANGGAL=$(date +"%Y%m%d-%H")
 export LC_ALL=C && export USE_CCACHE=1
 export ARCH=arm64
 if [ ! -d "clang" ]; then
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-14.0.0_r2/clang-r487747c.tar.gz -O "aosp-clang.tar.gz"
-    mkdir clang && tar -xf aosp-clang.tar.gz -C clang && rm -rf aosp-clang.tar.gz
+    git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git clang --depth=1
 fi
 
 [ -d "out" ] && rm -rf out || mkdir -p out
@@ -25,7 +24,7 @@ make -j$(nproc --all) O=out \
 
 function zipping()
 {
-git clone --depth=1 https://github.com/Amritorock/AnyKernel3 AnyKernel
+git clone --depth=1 https://github.com/Amritorock/AnyKernel3 -b r5x AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
 zip -r9 Stormbreaker-r5x-${TANGGAL}.zip *
